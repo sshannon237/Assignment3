4f3f39dd497488402fa5c0fc6134507a87ee6e4b
sam shannon <sshannon237@gmail.com>
2022-01-27 20:01:09 -0800

    Adding test project

---
+++ Assignment3Tests/App_Start/FilterConfig.cs
@@ -0,0 +1,10 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Assignment3Tests {
+    public class FilterConfig {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters) {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Assignment3Tests/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Assignment3Tests.MvcApplication" Language="C#" %>
