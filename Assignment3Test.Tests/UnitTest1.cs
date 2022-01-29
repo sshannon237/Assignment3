05e67b162083ca0cd40645df45c23f2df0e20b13
Jimun <wkdwlans009@gmail.com>
2022-01-29 14:41:48 -0800

    UnitTestService separate file added for selenium testing on service

--- Assignment3Test.Tests/UnitTest1.cs
+++ Assignment3Test.Tests/UnitTest1.cs
@@ -1,11 +1,73 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
-namespace Assignment3Test.Tests {
+namespace Assignment3Test.Tests
+{
+
     [TestClass]
-    public class UnitTest1 {
+    public class ServiceTests
+    {
+        string url = "http://localhost:55544";
+        ChromeDriver driver = new ChromeDriver();
+
+        public ServiceTests()
+        {
+        }
+
         [TestMethod]
-        public void TestMethod1() {
+        public void TestCreateService()
+        {
+            driver.Navigate().GoToUrl(url);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            driver.FindElement(By.LinkText("Service")).Click();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            driver.FindElement(By.LinkText("Create New")).Click();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            driver.FindElement(By.Id("Name")).SendKeys("Renting");
+            driver.FindElement(By.XPath("//Input[@type='submit']")).Click();
+        }
+
+        [TestMethod]
+
+        public void TestEditService()
+        {
+            driver.Navigate().GoToUrl(url);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            driver.FindElement(By.LinkText("Service")).Click();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            driver.FindElement(By.LinkText("Edit")).Click();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            driver.FindElement(By.Id("Name")).Clear();
+            driver.FindElement(By.Id("Name")).SendKeys("Online Purchase");
+            driver.FindElement(By.XPath("//Input[@type='submit']")).Click();
+        }
+
+        [TestMethod]
+
+        public void TestReadService()
+        {
+            driver.Navigate().GoToUrl(url);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            driver.FindElement(By.LinkText("Service")).Click();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            driver.FindElement(By.LinkText("Details")).Click();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            driver.FindElement(By.XPath("//*[contains(text(), 'Online Purchase')]"));
+        }
+
+        [TestMethod]
+
+        public void TestDeleteService()
+        {
+            driver.Navigate().GoToUrl(url);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            driver.FindElement(By.LinkText("Service")).Click();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            driver.FindElement(By.LinkText("Delete")).Click();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            driver.FindElement(By.XPath("//Input[@type='submit']")).Click();
         }
     }
 }
